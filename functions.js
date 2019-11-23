ca6a747e0c6bead8dea11688e5886ccd4f049c4b
github username <Lukabaramishvili@gmail.com>
2019-11-23 11:04:00 +0100

    add tests for user fetching with promise and async

--- functions.js
+++ functions.js
@@ -1,6 +1,17 @@
+const axios = require('axios');
+
 const functions = {
-    add: (num1, num2) => num1 + num2,
-    isNull: () => null
+  add: (num1, num2) => num1 + num2,
+  isNull: () => null,
+  checkValue: (x) => x,
+  createUser: () => {
+    const user = { firstName: 'Luka' };
+    user['lastName'] = 'Baramishvili';
+    return user;
+  },
+  fetchUser: () => axios.get('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.data)
+    .catch(err => 'error')
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -11,3 +11,47 @@ test('Adds 2 + 2 to NOT equal 5', () => {
 test('Should be null', () => {
     expect(functions.isNull()).toBeNull();
 })
+
+test('Should be falsy', () => {
+    expect(functions.checkValue(null)).toBeFalsy();
+})
+
+test('User should be Luka Baramishvili object', () => {
+    expect(functions.createUser()).toEqual({ firstName: 'Luka', lastName: 'Baramishvili' });
+  });
+
+//Less than and greater than
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 800;
+    expect(load1 + load2).toBeLessThanOrEqual(1600);
+  });
+
+  //regex (what if I want to make it case sensitive)
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/I/i);
+  });
+  
+  //Arrays
+  test('Admin should be in usernames array', () => {
+    usernames = ['john', 'kate', 'admin'];
+    expect(usernames).toContain('admin');
+  })
+
+
+//working with async data
+
+  //Promise
+// test('User fetched name should be Leanne Graham', () => {
+//   expect.assertions(1);
+//   return functions.fetchUser().then(data => {
+//       expect(data.name).toEqual('Leanne Graham');
+//     });
+// });
+
+//Async Await
+test('User fetched name should be Leanne Graham', async() => {
+    expect.assertions(1);
+    const data = await functions.fetchUser();
+    expect(data.name).toEqual('Leanne Graham');
+  });
